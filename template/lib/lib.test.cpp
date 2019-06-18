0b59263cb9ce479737f8cfaf5556779b64f9b75f
XzoRit <xzorit@hotmail.com>
2019-06-18 18:53:24 +0200

    use anonymous namepace test suite for test code

--- template/lib/lib.test.cpp
+++ template/lib/lib.test.cpp
@@ -1,8 +1,15 @@
 #include <lib/lib.hpp>
 #include <boost/test/unit_test.hpp>
 
+namespace
+{
+BOOST_AUTO_TEST_SUITE(lib_tests)
+
 BOOST_AUTO_TEST_CASE(lib_add)
 {
   const auto a{ xzr::lib::add(1, 3) };
   BOOST_TEST(a != 4);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
 }
