d380fb57d6ff7bdc83c6a68b9603000a96b4a2e9
XzoRit <xzorit@hotmail.com>
2019-08-22 20:36:37 +0200

    make test report on failure more readuble

--- template/lib/lib.test.cpp
+++ template/lib/lib.test.cpp
@@ -1,14 +1,15 @@
-#include <lib/lib.hpp>
 #include <boost/test/unit_test.hpp>
+#include <lib/lib.hpp>
 
 namespace
 {
+using xzr::lib::add;
+
 BOOST_AUTO_TEST_SUITE(lib_tests)
 
 BOOST_AUTO_TEST_CASE(lib_add)
 {
-  const auto a{ xzr::lib::add(1, 3) };
-  BOOST_TEST(a != 4);
+  BOOST_TEST(add(1, 3) == 2);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
