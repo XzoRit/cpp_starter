6ee49c16f0b710a84ec7e0a8db2cf074ec77926f
XzoRit <xzorit@hotmail.com>
2020-02-08 12:56:08 +0100

    change format style of c++-sources

--- lib/lib/lib.hpp
+++ lib/lib/lib.hpp
@@ -1,12 +1,9 @@
 #pragma once
 
-namespace xzr
-{
-namespace lib
-{
-inline namespace v1
-{
-int add(int a, int b);
-}
-}
-}
+namespace xzr {
+namespace lib {
+    inline namespace v1 {
+        int add(int a, int b);
+    }
+} // namespace lib
+} // namespace xzr
--- lib/lib/lib.test.cpp
+++ lib/lib/lib.test.cpp
@@ -1,16 +1,13 @@
-#include <boost/test/unit_test.hpp>
 #include <lib/lib.hpp>
 
-namespace
-{
+#include <boost/test/unit_test.hpp>
+
+namespace {
 using xzr::lib::add;
 
 BOOST_AUTO_TEST_SUITE(lib_tests)
 
-BOOST_AUTO_TEST_CASE(lib_add)
-{
-  BOOST_TEST(add(1, 3) == 2);
-}
+BOOST_AUTO_TEST_CASE(lib_add) { BOOST_TEST(add(1, 3) == 2); }
 
 BOOST_AUTO_TEST_SUITE_END()
-}
+} // namespace
