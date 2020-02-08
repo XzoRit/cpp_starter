134153db9a4862a7a9d1e98a8d43f781bd1dde39
XzoRit <xzorit@hotmail.com>
2020-02-08 17:24:41 +0100

    add travis-ci support

--- lib/lib/lib.test.cpp
+++ lib/lib/lib.test.cpp
@@ -7,7 +7,7 @@ using xzr::lib::add;
 
 BOOST_AUTO_TEST_SUITE(lib_tests)
 
-BOOST_AUTO_TEST_CASE(lib_add) { BOOST_TEST(add(1, 3) == 2); }
+BOOST_AUTO_TEST_CASE(lib_add) { BOOST_TEST(add(1, 3) == 4); }
 
 BOOST_AUTO_TEST_SUITE_END()
 } // namespace
