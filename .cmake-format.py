6d650a64dad2df114ca4f191a048068c7602e860
XzoRit <xzorit@hotmail.com>
2020-02-08 13:02:08 +0100

    cmake build files formatting

--- .cmake-format.py
+++ .cmake-format.py
@@ -5,7 +5,7 @@ line_width = 80
 tab_size = 2
 
 # If arglists are longer than this, break them always
-max_subargs_per_line = 1
+max_subargs_per_line = 0
 
 # If true, separate flow control names from their parentheses with a space
 separate_ctrl_name_with_space = False
