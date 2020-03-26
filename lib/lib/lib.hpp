b0350138a501d2c78a9f2ddf03eeebadd92db3af
XzoRit <xzorit@hotmail.com>
2020-03-26 16:11:00 +0100

    add documentation to lib

--- lib/lib/lib.hpp
+++ lib/lib/lib.hpp
@@ -6,7 +6,10 @@ namespace lib
 {
 inline namespace v1
 {
+/// \brief returns the sum of a and b.
+///
+/// Just a simple sumation function with a nice documentation.
 int add(int a, int b);
-}
+} // namespace v1
 } // namespace lib
 } // namespace xzr
