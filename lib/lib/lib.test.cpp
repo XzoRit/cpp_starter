822b7cf4849ab541ff56969f51dc8f673ff18f40
XzoRit <xzorit@hotmail.com>
2024-04-01 12:55:37 +0200

    make test fail by default

--- lib/lib/lib.test.cpp
+++ lib/lib/lib.test.cpp
@@ -10,7 +10,8 @@ BOOST_AUTO_TEST_SUITE(lib_tests)
 
 BOOST_AUTO_TEST_CASE(lib_add)
 {
-    BOOST_TEST(add(1, 3) == 4);
+    BOOST_TEST(add(2, 2) == 4);
+    BOOST_TEST(add(2, 3) == 4);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
